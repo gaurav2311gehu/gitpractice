629dbb5bf088f6f3878d304f457b3cebf7d68032
Practics <sainig273@gmail.com>
2025-08-31 10:33:51 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,8 @@
+# Small Python app for Git branching practice
+
+def greet(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    print(greet(user_name))
